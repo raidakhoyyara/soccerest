2c6c32057b90f92f9bfe52db301a155c24868a4c
raidakhoyyara <raidakhoyyara247@gmail.com>
2025-09-09 13:33:43 +0700

    Fix: add main app routing

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
-from main.views import show_main
-
+from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('', include('main.urls')),
+    path('', views.show_main, name='show_main'),
 ]
